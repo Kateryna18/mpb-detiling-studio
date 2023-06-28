e42b41d42f5bd1a634a0e34fdb99dabed879b438
Kateryna18 <mikolaichukkate@icloud.com>
2023-06-28 14:18:55 +0300

    added swiper in section about

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,4 +1,29 @@
 new Swiper('.swiper', {
+    slidesPerView: 2,
+    spaceBetween: 10,
+    centeredSlides: true,
+    effect: 'coverflow',
+    autoHeight: true,
+    // при зміні розмру екрану - відключити
+    // coverflowEffect: {
+    //     rotate: 20,
+    //     stretch: 5,
+    //     slideShadows: true,
+    // },
+    slideToClickedSlide: true,
+    loop: true,
+    speed: 800,
+    lazyPreloadPrevNext: 1,
+    watchSlidesProgress: true,
+    watchSlidesVisibility: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
